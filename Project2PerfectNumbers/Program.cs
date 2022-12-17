330282993493b768e9da6b858f0944abc5c94d44
Amr Bedir <amr.bedir.1@gmail.com>
2022-12-17 11:50:06 +0200

    Add files via upload

---
+++ Project2PerfectNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public class Project2
+{
+    public static void Main()
+    {
+        int n, i, sum;
+        int Number1, Number2;
+
+        Console.Write("\n\n");
+        Console.Write("Perfect Numbers in a range from N1 to N2:\n");
+        Console.Write("------------------------------------------------------");
+        Console.Write("\n\n");
+
+        Console.Write("Enter N1: ");
+        Number1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter N2: ");
+        Number2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("\nPerfect Numbers between the given range are: \n");
+        for (n = Number1; n <= Number2; n++)
+        {
+            i = 1;
+            sum = 0;
+            while (i < n)
+            {
+                if (n % i == 0)
+                    sum = sum + i;
+                i++;
+            }
+            if (sum == n)
+                Console.Write("{0} ", n);
+        }
+        Console.Write("\n");
+    }
+}
